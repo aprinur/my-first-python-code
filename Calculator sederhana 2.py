c17e70c9118cb77ab3dfaaec2c222e2549273aad
Apri Nur <aprinur7@gmail.com>
2024-04-07 14:02:56 +0700

    Trial buat Calculator sederhana 2

--- Calculator sederhana 2.py
+++ Calculator sederhana 2.py
@@ -1,50 +1,32 @@
-
 def kalkulasi():
 
     while True:
         a = float(input('\nMasukkan nilai yang akan dikalkulasi :'))
         c = input('Masukkan lambang operator(+,-,*,/,%,//) : ')
-        b = float(input('Masukkan nilai pengkalkulasi : '))
-
-        if c == '+':
-            d = a + b
-            print(d)
-            return d
-        elif c == '-':
-            d = a - b
-            print(d)
-            return d
-        elif c == '*':
-            d = a * b
-            print(d)
-            return d
-        elif c == '/':
-            d = a / b
-            print(d)
-            return d
-        elif c == '%':
-            d = a % b
-            print(d)
-            return d
-        elif c == '//':
-            d = a // b
-            print(d)
-            return d
-
+        if c not in ['+', '-', '*', '/', '%', '//']:
+            print('Lambang operator yang dimasukkan salah')
+            continue
         else:
-            print('Lambang operator tidak valid')
-            return
+            b = float(input('Masukkan nilai pengkalkulasi : '))
 
+            if c == '+':
+                d = a + b
+            elif c == '-':
+                d = a - b
+            elif c == '*':
+                d = a * b
+            elif c == '/':
+                d = a / b
+            elif c == '%':
+                d = a % b
+            elif c == '//':
+                d = a // b
 
-print('\n')
+            print('Hasil kalkulasi : ', d)
+
+            lanjutkan = input('\n1. Lanjut kalkulasi\n2. Hentikan Kalkulasi \nMasukkan opsi yang dipilih (1/2): ')
+            if lanjutkan == '2':
+                print('Kalkulasi dihentikan')
+                break
 
 kalkulasi()
-
-print('\n1. Lanjut kalkulasi ')
-print('2. Stop')
-
-x = input('Masukkan opsi yang dipilih (1/2) : ')
-if x == '1':
-    kalkulasi()
-else:
-    print('Kalkulasi dihentikan')
