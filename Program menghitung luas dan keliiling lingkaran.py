05e7da85f339ce34009cd3d8ff8b7218382a2f68
Apri Nur <aprinur7@gmail.com>
2024-04-25 14:44:37 +0700

    Membuat program hitung luas dan kelilling lingkaran

---
+++ Program menghitung luas dan keliiling lingkaran.py
@@ -0,0 +1,7 @@
+print('Menghitung Luas dan keliling lingkaran\n')
+radius = float(input('Masukkan nilai radius: '))
+luas_lingkaran = 3.14 * radius ** 2
+keliling_lingkaran = 2 * 3.14 * radius
+
+print(f'Luas lingkaran adalah {luas_lingkaran}'
+      f' dan kelilingnya adalah {keliling_lingkaran}')
