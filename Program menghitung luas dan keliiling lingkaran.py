94f1ea46e2e7d5a0e693b801b67d875ce2391510
Apri Nur <aprinur7@gmail.com>
2024-04-29 21:41:14 +0700

    add satuan

--- Program menghitung luas dan keliiling lingkaran.py
+++ Program menghitung luas dan keliiling lingkaran.py
@@ -1,7 +1,7 @@
-print('Menghitung Luas dan keliling lingkaran\n')
+print('\nMenghitung Luas dan keliling lingkaran\n')
 radius = float(input('Masukkan nilai radius: '))
+satuan = input("Masukkan satuan: \n")
 luas_lingkaran = 3.14 * radius ** 2
 keliling_lingkaran = 2 * 3.14 * radius
 
-print(f'Luas lingkaran adalah {luas_lingkaran}'
-      f' dan kelilingnya adalah {keliling_lingkaran}')
+print(f'Luas lingkaran adalah {luas_lingkaran} {satuan} dan kelilingnya adalah {keliling_lingkaran} {satuan}')
